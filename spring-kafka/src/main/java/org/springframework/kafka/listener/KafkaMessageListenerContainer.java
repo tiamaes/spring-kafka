ed6523be06045ab8d39ba91b17005a082f41d15e
Gary Russell <grussell@pivotal.io>
2016-08-03 20:22:38 -0400

    GH-161; Memory Leak with autoCommit
    Fixes #161
    
    We should not add to the `acks` collection when autoCommit is true.
    
    Polishing - PR Comments
    (cherry picked from commit 9e0e248)

--- spring-kafka/src/main/java/org/springframework/kafka/listener/KafkaMessageListenerContainer.java
+++ spring-kafka/src/main/java/org/springframework/kafka/listener/KafkaMessageListenerContainer.java
@@ -600,7 +600,7 @@ public class KafkaMessageListenerContainer<K, V> extends AbstractMessageListener
 					else {
 						this.listener.onMessage(record);
 					}
-					if (!this.isAnyManualAck) {
+					if (!this.isAnyManualAck && !this.autoCommit) {
 						this.acks.add(record);
 					}
 					if (this.isRecordAck) {
@@ -608,7 +608,7 @@ public class KafkaMessageListenerContainer<K, V> extends AbstractMessageListener
 					}
 				}
 				catch (Exception e) {
-					if (this.containerProperties.isAckOnError()) {
+					if (this.containerProperties.isAckOnError() && !this.autoCommit) {
 						this.acks.add(record);
 					}
 					if (this.containerProperties.getErrorHandler() != null) {
@@ -848,6 +848,9 @@ public class KafkaMessageListenerContainer<K, V> extends AbstractMessageListener
 			@Override
 			public void acknowledge() {
 				try {
+					if (ListenerConsumer.this.autoCommit) {
+						throw new IllegalStateException("Manual acks are not allowed when auto commit is used");
+					}
 					ListenerConsumer.this.acks.put(this.record);
 				}
 				catch (InterruptedException e) {
