c7038ae0e80b08d9e789059707f22528172dab1e
Gary Russell <grussell@pivotal.io>
2016-08-12 12:17:29 -0400

    GH-164: Document Log Compaction Support
    Resolves #164 (https://github.com/spring-projects/spring-kafka/issues/164)
    
    Also fix message conversion for `send(Message<?> m)`.
    (cherry picked from commit 632559a)

--- spring-kafka/src/test/java/org/springframework/kafka/annotation/EnableKafkaIntegrationTests.java
+++ spring-kafka/src/test/java/org/springframework/kafka/annotation/EnableKafkaIntegrationTests.java
@@ -57,6 +57,8 @@ import org.springframework.kafka.listener.adapter.RetryingAcknowledgingMessageLi
 import org.springframework.kafka.listener.config.ContainerProperties;
 import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.kafka.support.KafkaNull;
+import org.springframework.kafka.support.TopicPartitionInitialOffset;
 import org.springframework.kafka.support.converter.StringJsonMessageConverter;
 import org.springframework.kafka.test.rule.KafkaEmbedded;
 import org.springframework.kafka.test.utils.KafkaTestUtils;
@@ -187,7 +189,8 @@ public class EnableKafkaIntegrationTests {
 
 	@Test
 	public void testMulti() throws Exception {
-		template.send("annotated8", 0, "foo");
+		template.send("annotated8", 0, 1, "foo");
+		template.send("annotated8", 0, 1, null);
 		template.flush();
 		assertThat(this.multiListener.latch1.await(60, TimeUnit.SECONDS)).isTrue();
 	}
@@ -539,10 +542,15 @@ public class EnableKafkaIntegrationTests {
 	@KafkaListener(id = "multi", topics = "annotated8")
 	static class MultiListenerBean {
 
-		private final CountDownLatch latch1 = new CountDownLatch(1);
+		private final CountDownLatch latch1 = new CountDownLatch(2);
 
 		@KafkaHandler
 		public void bar(String bar) {
+			latch1.countDown();
+		}
+
+		@KafkaHandler
+		public void bar(@Payload(required = false) KafkaNull nul, @Header(KafkaHeaders.RECEIVED_MESSAGE_KEY) int key) {
 			latch1.countDown();
 		}
 
